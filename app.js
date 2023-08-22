129e3755d423764b65a68d4e3ebd46c25bcee643
Manuel Garcia <manugdesigner@gmail.com>
2023-08-22 23:03:41 +0200

    Navbar creado y probando si cambia las paginas

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path'); // Agrega esta línea para importar el módulo path
+const app = express();
+const port = 3000;
 
-// Servir archivos estáticos desde la carpeta "public"
-app.use('/static', express.static('public'));
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
-})
+});
 
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/public/about.html');
-})
+});
 
 app.get('/proyectos', (req, res) => {
   res.sendFile(__dirname + '/public/proyectos.html');
-})
+});
 
 app.get('/contacto', (req, res) => {
-    res.sendFile(__dirname + '/public/contacto.html');
-})
+  res.sendFile(__dirname + '/public/contacto.html');
+});
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`)
-})
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
